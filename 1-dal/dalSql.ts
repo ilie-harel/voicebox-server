94114d534ae4d266949cdfb7ddb03e6ef5937cd6
ilieBeracha <ilieberacha123@gmail.com>
2023-03-09 21:51:53 +0200

    port

--- 1-dal/dalSql.ts
+++ 1-dal/dalSql.ts
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    port: 3306,
+    port: 1433,
     database: process.env.DB_NAME
 });
 
